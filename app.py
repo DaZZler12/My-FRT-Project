12deb5597d2fb74d11063b40030c8d8f17d242b7
DaZZler12 <78467862+DaZZler12@users.noreply.github.com>
2022-02-10 01:04:49 +0530

    msg

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask , render_template , request,flash,url_for
+import random,string
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 app = Flask(__name__)
@@ -50,52 +51,23 @@ def about():
 
 #here is the code that works well with the flask flashing message flash is shown properly
 
-@app.route('/schedule_test',methods=['GET','POST'])
-def schedule_test():
+@app.route('/schedule',methods=['POST'])
+def schedule():
 
    if request.method=="POST":
       booked_data = request.form
       text_msg=''
       btn=''
 
-      #print(booked_data)
-      if(booked_data['fname']=='' or booked_data['lname']=='' or booked_data['email']=='' or booked_data['phonenumber']=='' or (booked_data['address1']=='' and booked_data['address2']=='') or booked_data['city']=='') or booked_data['state']=='' or booked_data['pin']=='' or booked_data['date']=='': 
-         btn = 'Proceed'
-         text_msg = 'Fill All The Fields To Book Your Slot.'
-
-         request_data = {
-                 'fname': booked_data['fname'],
-                 'lname': booked_data['lname'],
-                 'email': booked_data['email'],
-                 'phonenumber': booked_data['phonenumber'],
-                 'address1': booked_data['address1'],
-                 'address2': booked_data['address2'],
-                 'state': booked_data['state'],
-                 'pin': booked_data['pin'],
-                 'date': booked_data['date'],
-                 'text_msg':text_msg,
-                 'btn':btn
-             }
-         flash('Fields Empty!!','error') #message and followed by category is apssed in the flash message
-         
-      else:
-         btn='Done'
-         text_msg = 'ThankYou, Booked Date: '+booked_data['date']+'. You will be notified over email shortly!!'
-         msg = 'Hi, '+ booked_data['fname']
-         request_data = {
-                 'fname': booked_data['fname'],
-                 'lname': booked_data['lname'],
-                 'email': booked_data['email'],
-                 'phonenumber': booked_data['phonenumber'],
-                 'address1': booked_data['address1'],
-                 'address2': booked_data['address2'],
-                 'state': booked_data['state'],
-                 'pin': booked_data['pin'],
-                 'date': booked_data['date'],
+ 
+      btn='Done'
+      text_msg = 'ThankYou, Booked Date: '+booked_data['date']+'. You will be notified over email shortly!!'
+      msg = 'Hi, '+ booked_data['fname']
+      request_data = {
                   'text_msg':text_msg,
                   'btn':btn
              }
-         flash(msg,'success')
+      flash(msg,'success')
       #or
       #return redirect(url_for('schedule_test',**request_data))
       return render_template('schedule_test.html',**request_data)
@@ -105,5 +77,30 @@ def schedule_test():
 
 
 if __name__ == "__main__":
-   app.secret_key = '12345jojsrojoh9211'
+   result = ''.join((random.choice(string.ascii_lowercase) for x in range(20))) # run loop 
+   app.secret_key = result
    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+   '''
+booked_data = request.form
+      text_msg=''
+      btn=''
+
+ 
+      btn='Done'
+      text_msg = 'ThankYou, Booked Date: '+booked_data['date']+'. You will be notified over email shortly!!'
+      msg = 'Hi, '+ booked_data['fname']
+      request_data = {
+                  'text_msg':text_msg,
+                  'btn':btn
+             }
+      flash(msg,'success')
+   '''
